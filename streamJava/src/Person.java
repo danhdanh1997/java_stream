2e60b6d2b3074e349a4636056dd51a99f81d8061
dongxuandanh <danh.dong@xomad.com>
2023-07-12 08:40:19 +0700

    Huy anh add isAlone variable

--- streamJava/src/Person.java
+++ streamJava/src/Person.java
@@ -2,10 +2,12 @@ public class Person {
     private String name;
     private int age;
     private double salary;
+    private boolean isAlone;
 
-    Person(String name, double salary, int age) {
+    Person(String name, double salary, int age, boolean isAlone) {
         this.name = name;
         this.salary = salary;
         this.age = age;
+        this.isAlone = isAlone;
     }
 }
